2c9d9d27c1078a54fb56d0b3458e2af537d161c7
kaylabedwards05 <124181753+kaylabedwards05@users.noreply.github.com>
2023-11-04 22:12:54 -0400

    Add files via upload
    title page

--- main.py
+++ main.py
@@ -1,42 +1,33 @@
 from cmu_graphics import *
 from PIL import Image
-import random
-
+def distance(x0, y0, x1, y1):
+    return ((x1 - x0)**2 + (y1 - y0)**2)**0.5
 def onAppStart(app):
-    app.width = 1200
-    app.height = 800
-    app.row = 3
-    app.col = 4
-    app.stickerbook = stickerbookSetup(app.row, app.col)
-    app.url = "cmu://664537/26105090/frog_stickers.jpg"
-    app.allTasks = [["shower", "brush your teeth", "bubbly bath", "skincare"],
-                    ["get booba", "drink wotta", "make le pizza", "make hot choccy!!"], 
-                    ["sing", "play games", "talk to your friends (not about school >:()", "watch a show"]]
-    app.currTasks = []
-    app.infoButton = 10, 10
-    app.stickerButton = 30, 10
-    getCurrTasks(app)
-    
-def getCurrTasks(app):
-    for row in range(len(app.allTasks)):
-        currNum = random.randrange(0, len(app.allTasks[row]))
-        (app.currTasks).append((row, currNum))
+    app.image = Image.open('titlefrog.PNG')
+    app.image = CMUImage(app.image)
+    app.playbutton = Image.open('playbutton.PNG')
+    app.playbutton = CMUImage(app.playbutton)
+    app.frogbutton = Image.open('frogbutton.PNG')
+    app.frogbutton = CMUImage(app.frogbutton)
+    app.titlecard = True
+    app.angle = 0
+def redrawAll(app):
+    if app.titlecard == True:
+        drawImage(app.image,app.width/2,app.height/2, align='center')
+        drawCircle(1200 *2/3,450,165, fill = 'cornSilk' )
+        drawCircle(1200 *2/3,450,150, fill = 'violet' )
+        drawImage(app.frogbutton,535,165,rotateAngle=app.angle)
 
-def stickerbookSetup(rows, cols):
-    stickers = [[False]*cols for i in range(rows)]
-    return stickers
-
-def revealSticker(app, taskTuple):
-    app.stickers[taskTuple[0]][taskTuple[1]] == True
-    return(f"Sticker at {taskTuple[0]}, {taskTuple[1]} is now True" )
-
-def drawStickerBoxes(app):
-    for i in range(len(app.stickerbook)):
-        for j in range(len(app.stickerbook[i])):
-            if app.stickerbook[i][j]: # if sticker is already found
-                drawRect(x*25, y*25, x+20, y+20, fill = "pink")
-
+def onMousePress(app, mouseX, mouseY):
+    cx = app.width *2/3
+    cy = app.height/2
+    r =app.width/5
+    if distance(cx,cy,mouseX,mouseY) <= r:
+        app.titlecard = False
+def onStep(app):
+    app.angle += 5
 def main():
-    runApp()
-    
-main()
+    runApp(1200,800)
+
+if __name__ == '__main__':
+    main()  
